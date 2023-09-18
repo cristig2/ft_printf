31331283de67ac65317fd24b8c098f095e932955
Cristina Gallardo Garcia <cristig2@hotmail.es>
2023-09-18 09:50:08 +0100

    functions for words & numbers

---
+++ ft_printf.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crgallar <crgallar@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 12:32:17 by crgallar          #+#    #+#             */
+/*   Updated: 2023/08/29 12:32:17 by crgallar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// ARCHIVO DE ENCABEZADOS
+
+/* Directivas de preprocesador:
+Estas directivas aseguran que el contenido del archivo de encabezado solo se
+incluirá una vez, evitando problemas de redefinición en caso de que el archivo
+se incluya múltiples veces en diferentes partes del código. */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+/* Inclusión de bibliotecas estándar (#include):
+Se está incluyendo las bibliotecas estándar necesarias, para que las funciones
+y tipos definidos en estas bibliotecas estén disponibles en el archivo de
+encabezado. */
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/* Prototipos de funciones:
+Se declaran los prototipos de las funciones que se implementan en el proyecto.
+Cada prototipo enumera el tipo de retorno, el nombre de la función y los 
+parámetros que la función espera recibir. */
+
+void	ft_putcharacter_length(char character, int *length);
+void	ft_string(char *args, int *length);
+
+void	ft_number(int num, int *len);
+void	ft_pointer(size_t ptr, int *len);
+void	ft_hexadecimal(unsigned int x, int *length, char x_or_x);
+void	ft_unsigned_int(unsigned int u, int *length);
+
+#endif
---
+++ sources/ft_words.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_words.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crgallar <crgallar@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 14:46:15 by crgallar          #+#    #+#             */
+/*   Updated: 2023/08/30 14:46:15 by crgallar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/* La función ft_putcharacter_length simplemente imprime un carácter en la
+salida estándar utilizando la función write y aumenta el contador de longitud
+(len) para realizar un seguimiento de cuántos caracteres se han impreso.*/
+
+void	ft_putcharacter_length(char chartr, int *len)
+{
+	write(1, &chartr, 1);
+	(*len)++;
+}
+
+/* La función ft_string se encarga de imprimir cadenas de caracteres. Si arg
+es NULL, imprime la cadena "(null)". Si no es NULL, itera sobre cada carácter
+de la cadena y lo imprime utilizando la función ft_putcharacter_length.*/
+
+void	ft_string(char *arg, int *len)
+{
+	size_t	i;
+
+	i = 0;
+	if (!arg)
+	{
+		write(1, "(null)", 6);
+		(*len) += 6;
+		return 0;
+	}
+	while (arg[i] != '\0')
+	{
+		ft_putcharacter_length(arg[i], len);
+		i++;
+	}
+}
+
+/*
+int	main(void)
+{
+	char* str = "Name: Cristina";
+	int len = 0;
+	char newline = '\n';
+
+	ft_string(str, &len);
+	printf("%cLongitud de la variable str: %d", newline, len);
+	return (0);
+}
+*/
