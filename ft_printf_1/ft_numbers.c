93e578b537d161a12adb306f20488e469c03e6d1
Cristina Gallardo Garcia <cristig2@hotmail.es>
2023-10-09 16:59:16 +0100

    Versiones subidas correctamente

--- ft_printf_1/ft_numbers.c
+++ ft_printf_1/ft_numbers.c
@@ -6,7 +6,7 @@
 /*   By: crgallar <crgallar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 10:21:06 by crgallar          #+#    #+#             */
-/*   Updated: 2023/10/09 12:34:12 by crgallar         ###   ########.fr       */
+/*   Updated: 2023/10/04 13:27:37 by crgallar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,56 +69,4 @@ char	*ft_itoa(int n)
 		len--;
 	}
 	return (str);
-}
-
-/*La funcion ft_base_hexa:
-Hace la conversion de un numero decimal a hexadecimal. 
-Se crea un puntero a la cadena que representa los digitios hexadecimales.
-Si el número es mayor o igual a 16, la función se llama a sí misma de forma
-recursiva para convertir el cociente dividido menor de 16, dividiendo por 16.
-Escribe el carácter correspondiente al residuo de la división por 16 (hexa).
-Este proceso se realiza de manera recursiva hasta convertir todo el número en su
-representación hexadecimal.
- */
-int	ft_base_hexa(int num)
-{
-	char	*hexa;
-	int		len;
-
-	hexa = "0123456789abcdef";
-	len = 0;
-	if (num >= 16)
-		len += ft_base_hexa(num / 16);
-	write (1, &hexa[num % 16], 1);
-	len++;
-	return (len);
-}
-
-/*La funcion ft_pointer:
-Usa como parametro un tipo de datos sin signo para almacenar valores no 
-negativos y otro puntero que lo usa para medir la longitud.
-imprime 0x */
-
-int	ft_pointer(size_t pointer)
-{
-	int	len;
-
-	write (1, "0x", 2);
-	len += 2;
-	len = ft_base_hexa(pointer);
-	return (len);
-}
-int main() {
-    int decimal_number = 255;
-    size_t pointer_address = (size_t) &decimal_number;
-
-    write(1, "Decimal number: ", 16);
-    write(1, &decimal_number, sizeof(decimal_number));
-    write(1, "\n", 1);
-
-    write(1, "Pointer address: ", 17);
-    int length = ft_pointer(pointer_address);
-    write(1, "\n", 1);
-
-    return 0;
 }
